dc93351b3c64eba0a63dd40ff0d5f96cb5eaf811
Devin Fromond <74107804+Dfromond@users.noreply.github.com>
2023-12-26 14:43:19 -0500

    Add files via upload

---
+++ Homework 8/Sorting.java
@@ -0,0 +1,415 @@
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+/**
+ * Your implementation of various sorting algorithms.
+ *
+ * @author Devin Fromond
+ * @version 1.0
+ * @userid dfromond3
+ * @GTID 903761713
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class Sorting {
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be:
+     * in-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n^2)
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Array or comparator cannot be null.");
+        }
+    
+        int length = arr.length;
+        for (int i = 0; i < length- 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (comparator.compare(arr[j], arr[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                T temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+    
+
+    /**
+     * Implement cocktail sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n)
+     *
+     * NOTE: See pdf for last swapped optimization for cocktail sort. You
+     * MUST implement cocktail sort with this optimization
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void cocktailSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Array or comparator cannot be null");
+        }
+    
+        int left = 0;
+        int right = arr.length - 1;
+        int lastSwappedIndex = 0;
+    
+        while (left < right) {
+            // Sort from left to right
+            for (int i = left; i < right; i++) {
+                if (comparator.compare(arr[i], arr[i + 1]) > 0) {
+                    swap(arr, i, i + 1);
+                    lastSwappedIndex = i;
+                }
+            }
+            right = lastSwappedIndex;
+    
+            // Sort from right to left
+            for (int i = right; i > left; i--) {
+                if (comparator.compare(arr[i - 1], arr[i]) > 0) {
+                    swap(arr, i - 1, i);
+                    lastSwappedIndex = i;
+                }
+            }
+            left = lastSwappedIndex;
+        }
+    }
+    
+
+    /**
+     * Swaps two elements in an array of type T.
+     * 
+     * @param arr the array in which to swap the elements
+     * @param i the index of the first element to be swapped
+     * @param j the index of the second element to be swapped
+     * 
+     * @param <T> the type of the elements in the array
+    */
+    private static <T> void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(n log n)
+     *
+     * And a best case running time of:
+     * O(n log n)
+     *
+     * You can create more arrays to run merge sort, but at the end, everything
+     * should be merged back into the original T[] which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * Hint: If two data are equal when merging, think about which subarray
+     * you should pull from first
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array to be sorted
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Array or Comparator is null");
+        }
+        int len = arr.length;
+        if (len <= 1) {
+            return; // already sorted
+        }
+        // split arr into two halves
+        int mid = len / 2;
+        T[] left = (T[]) new Object[mid];
+        T[] right = (T[]) new Object[len - mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < len; i++) {
+            right[i - mid] = arr[i];
+        }
+    
+        // recursively sort the two halves
+        mergeSort(left, comparator);
+        mergeSort(right, comparator);
+    
+        // merge the two sorted halves back into arr
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (comparator.compare(left[i], right[j]) <= 0) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+    
+    
+
+    /**
+     * Implement kth select.
+     *
+     * Use the provided random object to select your pivots. For example if you
+     * need a pivot between a (inclusive) and b (exclusive) where b > a, use
+     * the following code:
+     *
+     * int pivotIndex = rand.nextInt(b - a) + a;
+     *
+     * If your recursion uses an inclusive b instead of an exclusive one,
+     * the formula changes by adding 1 to the nextInt() call:
+     *
+     * int pivotIndex = rand.nextInt(b - a + 1) + a;
+     *
+     * It should be:
+     * in-place
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n)
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not get full
+     * credit if you do not implement the one we have taught you!
+     *
+     * @param <T>        data type to sort
+     * @param k          the index to retrieve data from + 1 (due to
+     *                   0-indexing) if the array was sorted; the 'k' in "kth
+     *                   select"; e.g. if k == 1, return the smallest element
+     *                   in the array
+     * @param arr        the array that should be modified after the method
+     *                   is finished executing as needed
+     * @param comparator the Comparator used to compare the data in arr
+     * @param rand       the Random object used to select pivots
+     * @return the kth smallest element
+     * @throws java.lang.IllegalArgumentException if the array or comparator
+     *                                            or rand is null or k is not
+     *                                            in the range of 1 to arr
+     *                                            .length
+     */
+    public static <T> T kthSelect(int k, T[] arr, Comparator<T> comparator, Random rand) {
+        if (arr == null || comparator == null || rand == null || k < 1 || k > arr.length) {
+            throw new IllegalArgumentException("Invalid argument(s)");
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        while (true) {
+            if (left == right) {
+                return arr[left];
+            }
+            int pivotIndex = rand.nextInt(right - left + 1) + left;
+            pivotIndex = partition(arr, left, right, pivotIndex, comparator);
+            if (k == pivotIndex + 1) {
+                return arr[pivotIndex];
+            } else if (k < pivotIndex + 1) {
+                right = pivotIndex - 1;
+            } else {
+                left = pivotIndex + 1;
+            }
+        }
+    }
+    
+    /**
+     * Partitions a subarray of the input array around a pivot value using a comparator.
+     * 
+     * @param arr the input array of type T
+     * @param left the left index of the subarray to be partitioned
+     * @param right the right index of the subarray to be partitioned
+     * @param pivotIndex the index of the pivot value within the subarray
+     * @param comparator a comparator to compare elements of type T
+     * @return the final index of the pivot value after partitioning
+    */
+    private static <T> int partition(T[] arr, int left, int right, int pivotIndex, Comparator<T> comparator) {
+        T pivotValue = arr[pivotIndex];
+        swap(arr, pivotIndex, right);
+        int storeIndex = left;
+        for (int i = left; i < right; i++) {
+            if (comparator.compare(arr[i], pivotValue) < 0) {
+                swap(arr, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        swap(arr, storeIndex, right);
+        return storeIndex;
+    }
+    
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not get full
+     * credit if you do not implement the one we have taught you!
+     *
+     * Remember you CANNOT convert the ints to strings at any point in your
+     * code! Doing so may result in a 0 for the implementation.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(kn)
+     *
+     * And a best case running time of:
+     * O(kn)
+     *
+     * You are allowed to make an initial O(n) passthrough of the array to
+     * determine the number of iterations you need. The number of iterations
+     * can be determined using the number with the largest magnitude.
+     *
+     * At no point should you find yourself needing a way to exponentiate a
+     * number; any such method would be non-O(1). Think about how how you can
+     * get each power of BASE naturally and efficiently as the algorithm
+     * progresses through each digit.
+     *
+     * Refer to the PDF for more information on LSD Radix Sort.
+     *
+     * You may use ArrayList or LinkedList if you wish, but it may only be
+     * used inside radix sort and any radix sort helpers. Do NOT use these
+     * classes with other sorts. However, be sure the List implementation you
+     * choose allows for stability while being as efficient as possible.
+     *
+     * Do NOT use anything from the Math class except Math.abs().
+     *
+     * @param arr the array to be sorted
+     * @throws java.lang.IllegalArgumentException if the array is null
+     */
+    public static void lsdRadixSort(int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Array can't be null");
+        }
+        LinkedList<Integer>[] buckets = new LinkedList[19];
+        for (int i = 0; i < 19; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        int mod = 10;
+        int div = 1;
+        boolean cont = true;
+        while (cont) {
+            cont = false;
+            for (int num : arr) {
+                int bucket = num / div;
+                if (bucket / 10 != 0) {
+                    cont = true;
+                }
+                if (buckets[bucket % mod + 9] == null) {
+                    buckets[bucket % mod + 9] = new LinkedList<>();
+                }
+                buckets[bucket % mod + 9].add(num);
+            }
+            int arrIdx = 0;
+            for (LinkedList<Integer> bucket : buckets) {
+                if (bucket != null) {
+                    for (int num : bucket) {
+                        arr[arrIdx++] = num;
+                    }
+                    bucket.clear();
+                }
+            }
+            div *= 10;
+        }
+    }
+    
+    
+    
+
+    /**
+     * Implement heap sort.
+     *
+     * It should be:
+     * out-of-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(n log n)
+     *
+     * And a best case running time of:
+     * O(n log n)
+     *
+     * Use java.util.PriorityQueue as the heap. Note that in this
+     * PriorityQueue implementation, elements are removed from smallest
+     * element to largest element.
+     *
+     * Initialize the PriorityQueue using its build heap constructor (look at
+     * the different constructors of java.util.PriorityQueue).
+     *
+     * Return an int array with a capacity equal to the size of the list. The
+     * returned array should have the elements in the list in sorted order.
+     *
+     * @param data the data to sort
+     * @return the array with length equal to the size of the input list that
+     * holds the elements from the list is sorted order
+     * @throws java.lang.IllegalArgumentException if the data is null
+     */
+    public static int[] heapSort(List<Integer> data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>(data);
+        int[] sorted = new int[data.size()];
+        for (int i = 0; i < sorted.length; i++) {
+            sorted[i] = heap.poll();
+        }
+        return sorted;
+    }
+}
